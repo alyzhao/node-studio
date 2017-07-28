d6828c41713620b4f0ccb8a8914204f18fd562c5
captainZhao <995029442@qq.com>
2017-07-28 12:19:09 +0800

    resolve some problem at last night!

--- request-comment/comment.js
+++ request-comment/comment.js
@@ -30,14 +30,44 @@ const options = {
 	}
 };
 
+const imoocFormData = querystring.stringify({
+	'content': "老师讲的真心棒!!!",
+	'mid': 8837
+});
+
+const imoocOptions = {
+	hostname: 'www.imooc.com',
+	port: 80,
+	path: '/course/docomment',
+	method: 'POST',
+	headers: {
+		'Accept': 'application/json, text/javascript, */*; q=0.01',
+		'Accept-Encoding':'gzip, deflate',
+		'Accept-Language':'zh-CN,zh;q=0.8',
+		'Cache-Control':'no-cache',
+		'Connection':'keep-alive',
+		'Content-Length':imoocFormData.length,
+		'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+		'Cookie':'imooc_uuid=a40a2d31-dcdf-4321-9de9-b6326bd366bd; imooc_isnew_ct=1493096962; UM_distinctid=15cf2bc5db5543-0693fb43d57b18-5393662-100200-15cf2bc5db64ca; CNZZDATA1261110065=110790494-1498719523-null%7C1498719523; PHPSESSID=ll141d1mf5lljqs587d5b53ib0; channel=491b6f5ab9637e8f6dffbbdd8806db9b_phpkecheng; loginstate=1; apsid=NkYzIyMGEwODgyZDUzNjJkZTUxMjE1NTcxZWM4ZjUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANTYzNDc4OAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzMzc3Mjk3NzA2QHFxLmNvbQAAAAAAAAAAAAAAAAAAADQ1ZWVmYWNlYmUyN2JhN2E5ODQ3ZTg5OGE1MzM0OGY3Qa96WUGvelk%3DMW; last_login_username=3377297706%40qq.com; bdshare_firstime=1501214313413; IMCDNS=0; Hm_lvt_f0cfcccd7b1393990c78efdeebff3968=1501053016,1501053029,1501212194,1501212405; Hm_lpvt_f0cfcccd7b1393990c78efdeebff3968=1501214797; imooc_isnew=2; cvde=597aae74a8d09-128',
+		'Host':'www.imooc.com',
+		'Origin':'http://www.imooc.com',
+		'Pragma':'no-cache',
+		'Referer':'http://www.imooc.com/video/8837',
+		'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36',
+		'X-Requested-With':'XMLHttpRequest'
+	}
+}
 
 
-const request = http.request(options, (res) => {
+
+const request = http.request(imoocOptions, (res) => {
 	console.log(`状态码: ${res.statusCode}`);
 	console.log(`响应头: ${JSON.stringify(res.headers)}`);
-	res.setEncoding('utf8');
+	// res.setEncoding('utf8');
 	res.on('data', (chunk) => {
 		console.log(Buffer.isBuffer(chunk));
+		let resData = chunk.toString();
+		console.log('resData: ' + resData);
 	});
 
 	res.on('end', () => {
@@ -50,7 +80,7 @@ const request = http.request(options, (res) => {
 
 });
 
-request.write(postData);
+request.write(imoocFormData);
 request.end();
 
 
