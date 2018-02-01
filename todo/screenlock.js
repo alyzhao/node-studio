d4c2bb8577846a9f5e561dd14563ea927b1b8552
Allen Zhao <995029442@qq.com>
2018-02-01 19:04:52 +0800

    modify

--- todo/screenlock.js
+++ todo/screenlock.js
@@ -21,15 +21,14 @@ function dealPic(files, type) {
 				let birthtime = new Date(stat.birthtime).getTime();
 				let rs = fs.createReadStream(picPath);
 
-				let existsFloder = fs.existsSync(path.resolve(__dirname, './images'));
-
-				console.log(existsFloder)
+				let dirPath = path.resolve(process.cwd(), './images')
+				let existsFloder = fs.existsSync(dirPath);
 
 				if (!existsFloder) {
-					fs.mkdirSync(path.resolve(__dirname, './images'));
+					fs.mkdirSync(dirPath);
 				}
 				
-				let ws = fs.createWriteStream(path.resolve(__dirname, './images', birthtime + '.jpg'));
+				let ws = fs.createWriteStream(path.resolve(dirPath, birthtime + '.jpg'));
 				rs.pipe(ws);
 			} catch (e) {
 				console.log(e);
@@ -47,19 +46,18 @@ process.stdin.on('readable', () => {
 
 	let files = fs.readdirSync(targetPath);
 	if (typeof chunk === 'string' && chunk.slice(0, -2) === 'nocopy') {
-		process.stdout.write('输入的是 nocopy');
 		process.stdout.write(`在原文件夹中修改!\n`);
 		process.stdout.write(`原文件夹路径: ${targetPath}\n`);		
 		dealPic(files, 1);
 	}
-	if (typeof chunk === 'string' && chunk.slice(0, -2) === 'copy') {
-		process.stdout.write('输入的是 copy');
+	if (typeof chunk === 'string' && chunk.slice(0, -2) !== 'nocopy') {
 		process.stdout.write(`复制到新文件夹当中!\n`);
 		process.stdout.write(`新文件夹路径: 当前目录下的 images 文件夹下!\n`);		
+		const dirPath = path.resolve(__dirname, './images');
+		console.log('dirPath', dirPath);
 		dealPic(files, 0);
 	}
 	if (typeof chunk === 'string' && chunk.slice(0, -2) === 'q') {
-		process.stdout.write('输入的是: q');
 		process.stdin.emit('end');
 	} 
 })
